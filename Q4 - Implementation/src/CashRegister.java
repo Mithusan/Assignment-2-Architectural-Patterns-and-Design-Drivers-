2d36f647c4923566ad8055f6f3b88dc4e4d4645f
Mohammad Mohammadkhani <65586276+CaptainMetal080@users.noreply.github.com>
2022-10-27 16:38:54 -0400

    Delete Q4 - Implementation directory

--- Q4 - Implementation/src/CashRegister.java
+++
@@ -1,8 +0,0 @@
-public class CashRegister {
-
-public String SearchProduct(String productName){
-    Database productDatabase=new Database();
-    Display screen=new Display();
-    return screen.PrintPrice(productDatabase.GetPrice(productName));
-}
-}
